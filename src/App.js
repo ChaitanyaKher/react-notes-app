d77671fa0e25209c391f3fd70d7539f41c3542b8
Chaitanya Kher <ckkher@amazon.com>
2023-01-13 22:34:00 +0530

    Added functionality to add new notes

--- src/App.js
+++ src/App.js
@@ -24,9 +24,20 @@ const App = () => {
     date: "16/01/2022"
   },
 ]);
+
+const addNote = (text) =>{
+  const date = new Date();
+		const newNote = {
+			id: nanoid(),
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+}
   return (
     <div className="container">
-      <NotesList notes = {notes} />
+      <NotesList notes = {notes} handleAddNote={addNote} />
     </div>
   )
 }
