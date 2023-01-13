4db7b80487aefd106c2f3a40dff839227324731e
Chaitanya Kher <ckkher@amazon.com>
2023-01-13 21:47:57 +0530

    Added dynamic notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { nanoid } from 'nanoid'
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes,setNotes] = useState([{
+    id: nanoid(),
+    text: "This is my First Note",
+    date: "13/01/2022"
+  },
+  {
+    id: nanoid(),
+    text: "This is my Second Note",
+    date: "14/01/2022"
+  },
+  {
+    id: nanoid(),
+    text: "This is my Third Note",
+    date: "15/01/2022"
+  },
+  {
+    id: nanoid(),
+    text: "This is my Fourth Note",
+    date: "16/01/2022"
+  },
+]);
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes = {notes} />
     </div>
   )
 }
